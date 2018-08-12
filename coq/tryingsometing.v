3abe885f8d11e27286d4bd2e6fdfa5e05ec40fe7
Nienke <n.wessel@xs4all.nl>
2018-08-12 17:57:00 +0200

    Cleaned up code

--- coq/tryingsometing.v
+++ coq/tryingsometing.v
@@ -25,19 +25,20 @@ Definition state := list (string * zext).
 
 (*Basically the definition of the fancy A*)
 Inductive aeval : state -> aexpr -> zext -> Prop :=
-ae_num : forall r n, aeval r (anum n) (val n) (*n evaluates to n*)
-| ae_var1 : forall r x v, aeval ((x,v)::r) (avar x) v
-| ae_var2 : forall r x y v v1 , x <> y -> aeval r (avar x) v1 ->
-aeval ((y,v)::r) (avar x) v1
+ae_num : forall s n, aeval s (anum n) (val n) (*n evaluates to n*)
+| ae_var1 : forall s x v, aeval ((x,v)::s) (avar x) v
+| ae_var2 : forall s x y v1 v2 , x <> y 
+-> aeval s (avar x) v2 
+-> aeval ((y,v1)::s) (avar x) v2
 | (*e1 + e2 evaluates to n1 + n2 if e1->n1 and e2->n2*) 
-ae_plus : forall r e1 e2 v1 v2 w1 w2, 
+ae_plus : forall s e1 e2 v1 v2 w1 w2, 
 v1 = (val w1) -> v2 = (val w2) ->
-aeval r e1 v1 -> aeval r e2 v2 ->
-aeval r (aplus e1 e2) (val (w1 + w2))
+aeval s e1 v1 -> aeval s e2 v2 ->
+aeval s (aplus e1 e2) (val (w1 + w2))
 | (*either e1 or e2 evaluates to nothing*) 
-ae_plus_fail : forall r e1 e2 v1 v2, 
+ae_plus_fail : forall s e1 e2 v1 v2, 
 v1 = nondec \/ v2 = nondec \/ v1 = dec \/ v2 = dec ->
-aeval r (aplus e1 e2) nondec
+aeval s (aplus e1 e2) nondec
 (*| (*Not encountered yet*)
 ae_doesntmatter : forall e, aeval nil e nondec*)
 .
@@ -45,11 +46,11 @@ ae_doesntmatter : forall e, aeval nil e nondec*)
 
 (*When we want to update the state, we call this function*)
 Inductive update : state -> string -> zext-> state -> Prop :=
-| s_up1 : forall r x v v1, update ((x,v)::r) x v1 ((x,v1):: r)
-| s_up2 : forall r r1 x y v v1, update r x v1 r1 ->
-x <> y -> update ((y,v)::r) x v1 ((y,v)::r1).
+| s_up1 : forall s x v v1, update ((x,v)::s) x v1 ((x,v1):: s)
+| s_up2 : forall s s1 x y v v1, update s x v1 s1 ->
+x <> y -> update ((y,v)::s) x v1 ((y,v)::s1).
 
-Definition usedvars := list string.
+(*Definition usedvars := list string.
 
 Inductive findvars : usedvars -> aexpr -> usedvars -> Prop :=
 | try1: forall l x, findvars l (avar x) (x::l) (*variables*)
@@ -57,33 +58,33 @@ Inductive findvars : usedvars -> aexpr -> usedvars -> Prop :=
 findvars l (aplus x y) l2(*addition*)
 | try3: forall l i, findvars l (anum i) l(*integers*)
 .
+*)
 
-
-Inductive updateforev : state -> aexpr -> state -> Prop :=
-| try11: forall l x l1, update l x nondec l1 -> updateforev l (avar x) l1 (*variables*)
-| try21: forall l x y l1 l2, (updateforev l x l1) -> (updateforev l1 y l2) -> 
-updateforev l (aplus x y) l2(*addition*)
-| try31: forall l i, updateforev l (anum i) l(*integers*)
+Inductive updateE : state -> aexpr -> state -> Prop :=
+| s_up3: forall s x s1, update s x nondec s1 -> updateE s (avar x) s1 (*variables*)
+| s_up4: forall s x y s1 s2, (updateE s x s1) -> (updateE s1 y s2) -> 
+updateE s (aplus x y) s2(*addition*)
+| s_up5: forall s i, updateE s (anum i) s(*integers*)
 .
 
 (*The actual natural semantics rules*)
 Inductive exec : state->instr->state->Prop :=
-| SN1 : forall r, exec r skip r (*skip rule*)
-| SN2 : forall r r1 x e v r2,
-aeval r e v -> update r x v r1 -> updateforev r1 e r2
--> aeval r (avar x) dec (*A[x]s = dec*)
--> not (aeval r e nondec)
--> exec r (assign x e) r2
+| exec_skip : forall s, exec s skip s (*skip rule*)
+| exec_ass : forall s x e v s1 s2,
+aeval s e v -> update s x v s1 -> updateE s1 e s2
+-> aeval s (avar x) dec (*A[x]s = dec*)
+-> not (aeval s e nondec)
+-> exec s (assign x e) s2
 (*assignment*)
-| SN4 : forall r x r1 r2 r3 y i a, (*s = r*)
-update r x dec r1 (*s[x |-> dec] = r1*)
--> exec r1 i r2 (*<S, r1> -> r2 = s'*)
--> aeval r (avar x) y (*r(x) = y = s(x)*)
--> update r2 x y r3 (*r3 = s'[x |-> s(x)]*)
--> exec r (let_in a x i) r3(*let-statement*)
-| SN3 : forall r r1 r2 i1 i2,
-exec r i1 r1 -> exec r1 i2 r2 
--> exec r (sequence i1 i2) r2 (*composition*). 
+| exec_let : forall s x s1 s2 s3 y i a, (*s is starting state*)
+update s x dec s1 (*s[x |-> dec] = s1*)
+-> exec s1 i s2 (*<S, s1> -> s2 = s'*)
+-> aeval s (avar x) y (*y = s(x)*)
+-> update s2 x y s3 (*s3 = s'[x |-> s(x)]*)
+-> exec s (let_in a x i) s3(*let-statement*)
+| exec_comp : forall s s1 s2 i1 i2,
+exec s i1 s1 -> exec s1 i2 s2 
+-> exec s (sequence i1 i2) s2 (*composition*). 
 
 
 (*assign z (anum 5)*)
@@ -92,10 +93,10 @@ Lemma Thalf:
 .
 Proof.
 intros z.
-apply SN2 with (r1 := ((z,val 5)::nil)) (v := (val 5)).
+apply exec_ass with (s1 := ((z,val 5)::nil)) (v := (val 5)).
 apply ae_num.
 apply s_up1.
-apply try31.
+apply s_up5.
 apply ae_var1.
 admit. (*I get stuck here!*)
 Qed.
@@ -111,12 +112,12 @@ Lemma T1:
 Proof.
 intros z. 
 intros a.
-apply SN4 with (r1 := ((z, dec)::nil)) (r2 := ((z, (val 5))::nil)) (y := nondec).
+apply exec_let with (s1 := ((z, dec)::nil)) (s2 := ((z, (val 5))::nil)) (y := nondec).
 apply s_up1.
-apply SN2 with (r1 := ((z, (val 5))::nil)) (v := (val 5)).
+apply exec_ass with (s1 := ((z, (val 5))::nil)) (v := (val 5)).
 apply ae_num.
 apply s_up1.
-apply try31.
+apply s_up5.
 apply ae_var1.
 unfold not.
 admit.
@@ -124,4 +125,11 @@ apply ae_var1.
 apply s_up1.
 Qed.
 
+
+Lemma eval_deterministic :
+ forall s e n n1, aeval s e n -> aeval s e n1 -> n = n1
+.
+Proof.
+admit.
+Qed.
 
